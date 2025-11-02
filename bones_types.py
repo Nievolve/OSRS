3f02322ce2c2b32911353a5e1767ebc9a8f3d871
Andreas <1990andreaslindblad@gmail.com>
2025-11-02 21:58:25 +0100

    first commit

---
+++ bones_types.py
@@ -0,0 +1,23 @@
+class bones:
+    prayerXP = 4
+    blessedBoneShard = 4
+    crafting = 5
+    
+class big_bones:
+    prayer = 15
+    blessedBoneShard = 12
+    crafting = 5
+
+class baby_dragon_bones:
+    prayer = 0
+    blessedBoneShard = 24
+    crafting = 5 
+
+class dragon_bones:
+    prayer = 0
+    blessedBoneShard = 58
+    crafting = 5
+class sun_kissed_bones:
+    prayer = 0
+    blessedBoneShard = 45
+    crafting = 5
